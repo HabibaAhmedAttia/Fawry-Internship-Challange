00acf512e13fe83edf0aa9f5f3c65d96d91a33d2
habiba <habibaahmedattia5@gmail.com>
2025-04-04 05:57:55 +0200

    test case

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import models.*;
+import services.CheckoutService;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        try {
+            Customer customer = new Customer("habiba", 500);
+
+            ShippableProduct cheese = new ShippableProduct("Cheese", 200, 5, 0.4);
+            ExpirableProduct biscuits = new ExpirableProduct("Biscuits", 150, 2, new Date(System.currentTimeMillis() + 100000));
+            Product scratchCard = new Product("Scratch Card", 50, 10) {};
+
+            Cart cart = new Cart();
+            cart.add(cheese, 1);
+            cart.add(biscuits, 1);
+            cart.add(scratchCard, 1);
+
+            CheckoutService.checkout(customer, cart);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
